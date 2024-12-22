8e324606d66b6489af5b77ffd9ad242c81c4d241
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2024-12-21 20:21:02 -0500

    Config site

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  site: 'https://www.pereiratechtalks.com',
+  site: 'https://matrimonio.xergioalex.com',
 
   server: {
     host: true,
