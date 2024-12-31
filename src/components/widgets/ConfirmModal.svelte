0a6ce3a30212710e51a5fd4a69d584ce649764ac
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2024-12-31 11:01:22 -0500

    RSVP section

--- src/components/widgets/ConfirmModal.svelte
+++ src/components/widgets/ConfirmModal.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount, createEventDispatcher } from 'svelte';
-  import type { Invite } from '~/types';
+  import type { Invite } from '~/types.d';
 
   export let invite: Invite;
 
